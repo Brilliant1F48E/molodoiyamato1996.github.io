77e805f520c157f714971d343c894dd8a376b890
molodoiyamato1996 <molodoi.yamato1996@gmail.com>
2022-11-18 20:45:12 +0700

    add get stream link

--- src/components/Live/Live.jsx
+++ src/components/Live/Live.jsx
@@ -1,18 +1,56 @@
+import { useState, useEffect } from 'react';
 import './Live.scss';
 
 
 const Live = () => {
-	const streamSrc = 'https://www.youtube.com/embed/3m4vdskBOns'
-	return (
-		<section className="live outer">
-			<div className="section-title">Прямая трансляция</div>
-			<div className="container">
-				<div className="live__wrapper">
-					<iframe className='live__stream' src={streamSrc} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+	const [day, setDay] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		fetch("api/days/getDay", {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			}
+		})
+			.then(response => response.json())
+			.then(
+				(result) => {
+					setIsLoaded(true)
+					setDay(result.day)
+				},
+				(error) => {
+					setIsLoaded(true)
+					setError(error.error)
+				});
+	}, [])
+
+	if (day) {
+		const streamLink = day.streamLink;
+
+		return (
+			<section className="live outer">
+				<div className="section-title">Прямая трансляция</div>
+				<div className="container">
+					<div className="live__wrapper">
+						<iframe className='live__stream' src={streamLink} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+					</div>
 				</div>
-			</div>
-		</section>
-	);
+			</section>
+		);
+	} else {
+		return (
+			<section className="live outer">
+				<div className="section-title">Прямая трансляция</div>
+				<div className="container">
+					<div className="live__wrapper">
+						<div className="live__notify">Трансляция в данный момент недоступа</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Live;
